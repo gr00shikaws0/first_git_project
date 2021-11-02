d151e70726c22ae424064133bb5d77e532c54279
Sergey <gr00shik@gmail.com>
2021-11-02 21:23:09 +0300

    clean the project

--- src/com/tms/condition/Main.java
+++ src/com/tms/condition/Main.java
@@ -4,10 +4,5 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!!!");
-
-        int i = 10;
-        int j = 5;
-        System.out.println(i/j);
-
     }
 }
