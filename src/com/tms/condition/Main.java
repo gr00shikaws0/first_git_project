700375b564ce45feac680ded5fa3c17be02fda5d
Sergey <gr00shik@gmail.com>
2021-11-02 21:00:07 +0300

    add additional class and improve Main class

--- src/com/tms/condition/Main.java
+++ src/com/tms/condition/Main.java
@@ -4,5 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!!!");
+
+        int i = 10;
+        int j = 5;
+        System.out.println(i/j);
+
     }
 }
---
+++ src/com/tms/condition/Test.java
@@ -0,0 +1,4 @@
+package com.tms.condition;
+
+public class Test {
+}
