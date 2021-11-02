297d0e9bcf667ebe89baa5d8bdfa6bc6748ff8ed
Sergey <gr00shik@gmail.com>
2021-11-02 21:21:07 +0300

    add sout to Test class with exit2 command

--- src/com/tms/condition/Test.java
+++ src/com/tms/condition/Test.java
@@ -5,7 +5,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hello Form Test");
         System.out.println("exit");
-        System.out.println("exit2");
+        System.out.println("exit3");
     }
 
 }
